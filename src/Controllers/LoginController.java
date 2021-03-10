d47dd385c947c87ac2e0837ea57b076aa7844cd9
dpound <dpound@naver.com>
2021-03-10 11:42:03 +0900

    뼈대 다시올림

---
+++ src/Controllers/LoginController.java
@@ -0,0 +1,35 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class LoginController implements Initializable{
+	private Parent root;
+	
+    public void setRoot(Parent root) {
+		this.root = root;
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+	
+	
+	public void btnLogin() {
+		
+	}
+	
+	public void btnJoin() {
+		
+	}
+
+
+	
+
+}
---
+++ src/Run/Run.java
@@ -0,0 +1,32 @@
+package Run;
+
+import Controllers.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Run extends Application{
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/LoginView.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		
+		//컨드롤러를 넣어주면 (fxid나 fxml에 있는 값들을 받아올수있어서 해놓은거임)
+		LoginController loginController = loader.getController();
+		loginController.setRoot(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
