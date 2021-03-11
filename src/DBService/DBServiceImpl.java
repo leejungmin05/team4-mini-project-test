cfa0d68c5f190182f565436861bf6bef4d7ce167
dpound <dpound@naver.com>
2021-03-11 09:42:38 +0900

    데베 기본틀도 올립니다

--- src/DBService/DBServiceImpl.java
+++ src/DBService/DBServiceImpl.java
@@ -1,6 +1,21 @@
 package DBService;
 
 public class DBServiceImpl implements DBService{
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 에러날시 개인컴에서 사용하던 url을 이용해주세요
+	private String id = ""; // 개인이 사용하시던 데베 아이디 이용해주세요
+	private String pwd = ""; // 비밀번호도 마찬가지!
+	
+	
+	public DBServiceImpl() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("드라이브 로드 되었습니다");
+	}
+	
+	
 
 	@Override
 	public void insert() {
